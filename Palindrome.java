755797b468a75a19356c7acfb9e680a4faf1267a
HaripriyaG <haripriyalavendar@gmail.com>
2016-09-02 11:08:27 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,28 @@
+# hari1
+import java.util.*;
+class Palindrome
+{
+public static void main(String [] arg)
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int sum=0;
+while(a>0)
+{
+int b=b%10;
+sum=sum+b;
+a=a/10;
+}
+String c=String.valueOf(sum);
+StringBuffer b1=new StringBuffer(c);
+String ans=b1.reverse().toString();
+if(c.equals(ans))
+{
+System.out.println("palindrome");
+}
+else
+{
+System.out.println("not a palindrome");
+}
+}
+}
